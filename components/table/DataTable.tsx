b8665a7b2d9769e63c29898e962fe9249a7b6ed3
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-10-03 23:56:41 -0300

    update

---
+++ components/table/DataTable.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import {
+  getPaginationRowModel,
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { decryptKey } from "@/lib/utils";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
---
+++ components/table/columns.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+
+import { Doctors } from "@/constants";
+import { formatDateTime } from "@/lib/utils";
+import { Appointment } from "@/types/appwrite.types";
+
+import { AppointmentModal } from "../AppointmentModal";
+import { StatusBadge } from "../StatusBadge";
